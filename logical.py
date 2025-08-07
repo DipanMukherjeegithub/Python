da83fdc6bcd8945dc97e82a6993b7bb7c7054f7d
DipanMukherjeegithub <dipanmukherjee2006@gmail.com>
2025-08-07 16:14:37 +0530

    ok

---
+++ logical.py
@@ -0,0 +1,9 @@
+# Input two numbers
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# Check if a is greater than or equal to b
+if a >= b:
+    print(True)
+else:
+    print(False)
